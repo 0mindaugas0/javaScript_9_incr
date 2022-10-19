b0fee869e81c0971021328fbbcf23f92c83df1b4
0mindaugas0 <mindaugas.mizaras@gmail.com>
2022-10-19 11:01:04 +0300

    pirma - penkta užduotys

---
+++ script9-1.js
@@ -0,0 +1,14 @@
+let skaicius;
+skaicius = +prompt("Įveskite skaičių");
+
+console.log(skaicius += 30);
+
+console.log(skaicius -= 57.239);
+
+console.log(skaicius *= 10);
+
+console.log(skaicius /= 2);
+
+console.log(skaicius += (-20));
+
+
